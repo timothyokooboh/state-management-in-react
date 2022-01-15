4b44b01825ffa7e474e42f667c13d4d461eaf924
timothy okooboh <timothyokooboh@seamlesshr.com>
2022-01-15 08:43:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn state management in React
         </a>
       </header>
     </div>
