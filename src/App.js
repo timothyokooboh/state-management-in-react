d0b31655f2ffdd7941f0342ea2766a4a5894f3df
timothy okooboh <okoobohtimothy@gmail.com>
2022-01-29 09:22:36 +0100

    learn use memo

--- src/App.js
+++ src/App.js
@@ -5,20 +5,34 @@ import Blog from './components/Blog';
 import AuthContextProvider from './context/AuthContext';
 import BlogContextProvider from './context/BlogContext';
 import UserContextProvider from './context/UserContext.js';
+import { useState, useMemo } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const expensive = useMemo(() => expensiveFunc(count))
+
   return (
     <div className="App">
+      <button onClick={() => setCount((count) => count + 1)}>click</button>
+      {count}
+      <div>expensive: {expensive}</div>
       <UserContextProvider>
         <AuthContextProvider>
           { /* <Home /> */ }
           <BlogContextProvider>
-            <Blog />
+          { /* <Blog /> */ }
           </BlogContextProvider>
         </AuthContextProvider>
       </UserContextProvider>
     </div>
   );
+}
+
+const expensiveFunc = (arg) => {
+  console.log("calculating>>>>")
+  for(let i = 0; i < 100000000000; i++) {
+    return i + arg * 5;
+  }
 }
 
 export default App;
